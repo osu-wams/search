a1a5e81d37d850e23849ad079a8e32cd1cbb73ff
Patenaude, Christopher T <patenauc@oregonstate.edu>
2019-06-24 16:12:21 -0700

    Added button to App for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
+import SearchButton from './SearchButton';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <SearchBar />
+      <SearchButton> Search OSU -></SearchButton>
     </div>
   );
 }
