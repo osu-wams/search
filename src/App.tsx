0b7579dc4cb102311a3f72f7c926cd9045deb935
Charles Koll <kollch@oregonstate.edu>
2019-06-25 12:12:00 -0700

    Rearrange result tags to correctly return results

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,20 @@ import styled from "styled-components";
 import "./App.css";
 import Search from "./Search";
 
-const Results = styled.div``;
-
 const App: React.FC = () => {
   useEffect(() => {
     if (!window.location.search.startsWith("?q=")) {
       window.location.search = "?q=hello";
     }
-    const cx = "001157565620839607635:f_5ovr-jasm";
-    const gcse = document.createElement("script");
-    gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
-    gcse.async = true;
-    document.head.appendChild(gcse);
   }, []);
 
   return (
     <div className="App">
       <Search />
-      <Results
+      <div
         className="gcse-searchresults-only"
         data-personalizedads="false"
-      ></Results>
+      ></div>
     </div>
   );
 };
