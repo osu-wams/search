283d8640d23d814515f1d1501d91afc97c9211ea
Charles Koll <kollch@oregonstate.edu>
2019-06-25 16:40:29 -0700

    Add fontawesome icons to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Search from "./Search";
 import Results from "./Results";
 
 const App: React.FC = () => {
-  /*
-  useEffect(() => {
-    if (!window.location.search.startsWith("?q=")) {
-      window.location.search = "?q=hello";
-    }
-  }, []);*/
-
   return (
     <div className="App">
       <Search />
