e443e6bc347f026cb4b394bf90f545d2c8f51276
Charles Koll <kollch@oregonstate.edu>
2019-06-25 09:11:37 -0700

    Add search results - Buggy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './App.css';
-import Search from './Search';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import "./App.css";
+import Search from "./Search";
+
+const Results = styled.div``;
 
 const App: React.FC = () => {
+  useEffect(() => {
+    if (!window.location.search.startsWith("?q=")) {
+      window.location.search = "?q=hello";
+    }
+    const cx = "001157565620839607635:f_5ovr-jasm";
+    const gcse = document.createElement("script");
+    gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
+    gcse.async = true;
+    document.head.appendChild(gcse);
+  }, []);
+
   return (
     <div className="App">
       <Search />
+      <Results
+        className="gcse-searchresults-only"
+        data-personalizedads="false"
+      ></Results>
     </div>
   );
-}
+};
 
 export default App;
