f4991e05515a5ac8e040874ae3b9db71151ff8d1
Patenaude, Christopher T <patenauc@oregonstate.edu>
2019-07-03 10:15:01 -0700

    Fix App component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.matchMedia = () => ({ 
+  addEventListener: () => {}, 
+  removeEventListener: () => {},
+  addListener: () => {}, 
+  removeListener: () => {},
+  matches: false,
+  media: "test",
+  onchange: () => {},
+  dispatchEvent: (event: Event) => {return false}
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
