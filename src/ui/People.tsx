e63d224d41901131ccee75bae367521b08e5f329
Charles Koll <kollch@oregonstate.edu>
2019-07-26 13:37:51 -0700

    Add loading state for people and places

--- src/ui/People.tsx
+++ src/ui/People.tsx
@@ -13,6 +13,23 @@ const PeopleIcon = styled(Icon)`
 
 const PersonName = styled.p`
   margin: 0;
+  font-size: ${theme.fontSize[16]};
+`;
+
+const PersonSkelName = styled(PersonName)`
+  @keyframes person-waiting {
+    from {
+      background-color: ${Color['neutral-100']};
+    }
+    to {
+      background-color: ${Color['neutral-200']};
+    }
+  }
+  background-color: ${Color['neutral-100']};
+  height: ${theme.fontSize[16]};
+  width: 85%;
+  margin: 4px 0;
+  animation: person-waiting 900ms infinite alternate;
 `;
 
 const PersonDept = styled.p`
@@ -20,12 +37,27 @@ const PersonDept = styled.p`
   font-size: ${theme.fontSize[12]};
 `;
 
+const PersonSkelDept = styled(PersonDept)`
+  @keyframes person-waiting {
+    from {
+      background-color: ${Color['neutral-100']};
+    }
+    to {
+      background-color: ${Color['neutral-200']};
+    }
+  }
+  background-color: ${Color['neutral-100']};
+  height: ${theme.fontSize[12]};
+  width: 70%;
+  margin: 3px 0;
+  animation: person-waiting 900ms infinite alternate;
+`;
+
 const PeopleList = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
   color: ${Color['neutral-700']};
-  font-size: ${theme.fontSize[16]};
   font-weight: 300;
 `;
 
@@ -64,28 +96,70 @@ const PeopleLink = styled.a`
   }
 `;
 
+const PeopleTotal = styled.p`
+  margin: 0;
+  margin-top: ${theme.spacing.unit * 2 + 2}px;
+  padding: 0;
+  font-size: ${theme.fontSize[14]};
+  color: ${Color['neutral-550']};
+  font-style: italic;
+  font-weight: 300;
+`;
+
+const PeopleTitle: React.FC = () => {
+  return (
+    <CardTitle>
+      <PeopleIcon icon={faUserCircle} color={Color['neutral-400']} />
+      People
+    </CardTitle>
+  );
+};
+
+const PeopleFooter = ({ href }) => {
+  return (
+    <CardFooter>
+      <PeopleLink href={href}>
+        OSU Directory
+        <Icon icon={faLongArrowRight} color={Color['orange-400']} />
+      </PeopleLink>
+    </CardFooter>
+  );
+};
+
 const PeopleEmptyState: React.FC = () => {
   return (
     <Card>
-      <CardTitle>
-        <PeopleIcon icon={faUserCircle} color={Color['neutral-400']} />
-        People
-      </CardTitle>
+      <PeopleTitle />
       <CardContent>
         <PeopleStatus>No people found.</PeopleStatus>
       </CardContent>
-      <CardFooter>
-        <PeopleLink href="http://directory.oregonstate.edu">
-          OSU Directory
-          <Icon icon={faLongArrowRight} color={Color['orange-400']} />
-        </PeopleLink>
-      </CardFooter>
+      <PeopleFooter href="http://directory.oregonstate.edu" />
+    </Card>
+  );
+};
+
+const PeopleWaiting: React.FC = () => {
+  return (
+    <Card>
+      <PeopleTitle />
+      <CardContent>
+        <PeopleList>
+          <Person>
+            <PersonSkelName />
+            <PersonSkelDept />
+          </Person>
+          <Person>
+            <PersonSkelName />
+            <PersonSkelDept />
+          </Person>
+        </PeopleList>
+      </CardContent>
     </Card>
   );
 };
 
 const People = ({ query }: { query: String }) => {
-  const [people, setPeople] = useState<any>([]);
+  const [people, setPeople] = useState<any>(null);
 
   useEffect(() => {
     request({
@@ -113,16 +187,15 @@ const People = ({ query }: { query: String }) => {
       });
   }, [query]);
 
-  if (!people.length) {
+  if (people === null) {
+    return <PeopleWaiting />;
+  } else if (!people.length) {
     return <PeopleEmptyState />;
   }
 
   return (
     <Card>
-      <CardTitle>
-        <PeopleIcon icon={faUserCircle} color={Color['neutral-400']} />
-        People
-      </CardTitle>
+      <PeopleTitle />
       <CardContent>
         <PeopleList>
           {people.map(person => {
@@ -140,13 +213,11 @@ const People = ({ query }: { query: String }) => {
             );
           })}
         </PeopleList>
+        <PeopleTotal>
+          Showing {people.length < 5 ? people.length : 5} of {people.length}
+        </PeopleTotal>
       </CardContent>
-      <CardFooter>
-        <PeopleLink href={`http://directory.oregonstate.edu/?type=search&cn=${query}`}>
-          OSU Directory
-          <Icon icon={faLongArrowRight} color={Color['orange-400']} />
-        </PeopleLink>
-      </CardFooter>
+      <PeopleFooter href={`http://directory.oregonstate.edu/?type=search&cn=${query}`} />
     </Card>
   );
 };
