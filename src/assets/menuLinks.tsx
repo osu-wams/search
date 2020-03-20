739f784de4354521e563373ea2bba31fd3ffc3c2
Mauricio Cordoba <mauricio.cordoba@oregonstate.edu>
2020-03-20 10:11:25 -0700

    cleans up menu code for readability and others improvements

--- src/assets/menuLinks.tsx
+++ src/assets/menuLinks.tsx
@@ -1,11 +1,11 @@
 const menuLinks = {
-  tophat: {
-    About: 'https://oregonstate.edu/about',
-    Academics: 'https://oregonstate.edu/academics',
-    Research: 'https://oregonstate.edu/osuresearch',
-    Outreach: 'https://outreach.oregonstate.edu',
-    Athletics: 'https://osubeavers.com'
-  }
+  tophat: [
+    { name: 'About', url: 'https://oregonstate.edu/about' },
+    { name: 'Academics', url: 'https://oregonstate.edu/academics' },
+    { name: 'Research', url: 'https://oregonstate.edu/osuresearch' },
+    { name: 'Outreach', url: 'https://outreach.oregonstate.edu' },
+    { name: 'Athletics', url: 'https://osubeavers.com' }
+  ]
 };
 
 export default menuLinks;
